fcbaaa16cf6112521f9972ad711dc0401ece98c8
Adrien B <forbu14@gmail.com>
2023-05-08 18:06:18 +0200

    adding model for openai embeddings

--- scripts/embedding_creation_openai.py
+++ scripts/embedding_creation_openai.py
@@ -17,8 +17,18 @@ from haystack.document_stores import FAISSDocumentStore
 from haystack.nodes import EmbeddingRetriever
 from haystack.schema import Document, FilterType
 
+import openai
 
-def create_embeddings(sentence, model):
+# read key.key file and set openai api key
+with open("../key.key", "r") as f:
+    key = f.read()
+
+# set api_key environment variable
+os.environ["api_key"] = key
+openai.api_key = os.environ["api_key"]
+
+
+def create_embeddings(sentences, model="text-embedding-ada-002"):
     """
     Simple function to create embeddings from a text.
 
@@ -31,26 +41,39 @@ def create_embeddings(sentence, model):
     """
 
     # Sentences are encoded by calling model.encode()
-    embedding = model.encode(sentence)
+    embedding = openai.Embedding.create(input=sentences, model=model)
 
-    return embedding
+    # retrieve all the embeddings in a list format
+    nb_embedding = len(embedding["data"])
+
+    list_embedding = []
+
+    for i in range(nb_embedding):
+        list_embedding.append(list(embedding["data"][i]["embedding"]))
+
+    return list_embedding
 
 
-def compute_embedding_full_text(texts, model):
+def compute_embedding_full_text(texts, batch_size=1000):
     """
     Compute the full text embedding for a list of texts.
     """
     embeddings_full_text = []
-    for text in tqdm(texts):
-        embedding = create_embeddings(text, model)
-        embeddings_full_text.append(embedding)
+
+    # we divide the texts in batch of batch_size
+    texts_batched = [
+        texts[i : i + batch_size] for i in range(0, len(texts), batch_size)
+    ]
+
+    for idx, texts in enumerate(tqdm(texts_batched)):
+        embedding = create_embeddings(texts)
+        embeddings_full_text += embedding
+
+        # save the embeddings in case of crash
+        with open(f"embeddings_full_text_tmp_{idx}.pickle", "wb") as handle:
+            pickle.dump(embeddings_full_text, handle)
 
     return embeddings_full_text
-
-
-def get_model():
-    model = SentenceTransformer("sentence-transformers/multi-qa-mpnet-base-dot-v1")
-    return model
 
 
 def read_data(path):
@@ -103,7 +126,9 @@ def create_faiss_document_store(documents, path_index, path_config):
     """
     Create and save faiss document store
     """
-    document_store = FAISSDocumentStore(duplicate_documents="overwrite", return_embedding=True)
+    document_store = FAISSDocumentStore(
+        duplicate_documents="overwrite", return_embedding=True
+    )
     document_store.write_documents(documents, duplicate_documents="overwrite")
     document_store.save(index_path=path_index, config_path=path_config)
 
@@ -111,20 +136,16 @@ def create_faiss_document_store(documents, path_index, path_config):
 
 
 if __name__ == "__main__":
-    # Load the model
-    print("Loading the model")
-    model = get_model()
-
     # Read the data
     print("Reading the data")
     data = read_data("../data_preprocess/")
 
-    # for testing
-    # data = data[:100]
+    # filter the data where there is nothing ''
+    data = [article for article in data if article != ""]
 
     # # Create embeddings
     print("Creating the embeddings")
-    embeddings = compute_embedding_full_text(data, model)
+    embeddings = compute_embedding_full_text(data)
 
     embeddings = np.array(embeddings)
 
@@ -132,6 +153,10 @@ if __name__ == "__main__":
     print("Saving the embeddings")
     with open("../embeddings.pickle", "wb") as handle:
         pickle.dump(embeddings, handle)
+
+    # # Create the documents
+    print("Creating the documents")
+    documents = create_documents_list(data, embeddings)
 
     # load pickle file
     with open("../documents.pickle", "rb") as handle:
